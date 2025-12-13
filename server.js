605d77d8fee77444a52bb8ba83db183ad487f2c9
Neeraj Goud <neerajgoud1919@gmail.com>
2025-12-13 19:18:16 +0530

    initial commit

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import sum from './sum.js';
 
 const app = express();
 
@@ -6,6 +7,12 @@ const app = express();
 app.get('/wish', (req, res) => {
     res.json({ message: 'Have a great day!' });
 });
+
+
+app.get('/sum/:a/:b', (req, res) => {
+  const{a , b}  = req.params;
+    res.json({ sum: sum(parseInt(a), parseInt(b))});
+})
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
---
+++ sum.test.js
@@ -0,0 +1,6 @@
+import sum from './sum.js';
+
+test('adds 3 + 2 to equal 5', () => {
+  expect(sum(3, 2)).toBe(5);
+});
+
