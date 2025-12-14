78c88eef9944d3d77c5637429b3ded032d5c02c9
Neeraj Goud <neerajgoud1919@gmail.com>
2025-12-14 22:14:18 +0530

    chages fun

--- sum.test.js
+++ sum.test.js
@@ -1,6 +1,17 @@
 import sum from './sum.js';
 
-test('adds 3 + 2 to equal 5', () => {
+
+describe('sum function', () => {
+
+ test('adds 3 + 2 to equal 5', () => {
   expect(sum(3, 2)).toBe(5);
-});
+ }); 
+
+  test('adds -1 + -2 to equal 5', () => {
+  expect(sum(-1, -2)).toBe(-3);
+ }); 
+
+
+})
+
 
